2d340cd9f0fc6651a8a955b0153a034df9ebdf49
Alain Campoverde <andresarcangel00@gmail.com>
2023-04-09 18:08:01 -0500

    mapeo de tablas del curso

--- src/main/java/com/darkross/mediappbackend/model/User.java
+++ src/main/java/com/darkross/mediappbackend/model/User.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "user_data")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
